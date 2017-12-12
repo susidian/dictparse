1d1524ab01ad230b56cfc40a1b26efb8f6350585
susidian <250127468@qq.com>
2017-12-12 15:35:00 +0800

    Update parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -1,19 +1,3 @@
-/***************************************************************************
- * 
- * Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
- * 
- **************************************************************************/
- 
- 
- 
-/**
- * @file parser.cpp
- * @author susidian(susidian@baidu.com)
- * @date 2017/11/08 19:42:51
- * @brief 
- *  
- **/
-
 #include "parser.h"
 
 namespace parser {
